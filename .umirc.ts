c4373cfd786635382e26ef4c38de997e4af546a9
mxszs <m1344993660@163.com>
2020-04-05 14:24:49 +0800

    feat: 编译前端代码

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   favicon: 'https://avatars2.githubusercontent.com/u/33783716?s=60&v=4',
   logo: 'https://avatars2.githubusercontent.com/u/33783716?s=60&v=4',
   navs: [null, { title: 'GitHub', path: 'https://github.com/mxszs' }],
-  base: './dist',
+  publicPath: './',
   menus: {
     '/guide': [
       {
